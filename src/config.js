c7c29ce4e13f6d38816bea3660074920f01e9883
Brandon Him <himbrandon1@gmail.com>
2021-08-20 16:26:42 -0400

    Remove config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ const oktaAuthConfig = {
     // you will need to add `pkce: false`
     // issuer: 'https://2020spaces-poc.okta.com/oauth2/default',
     issuer: 'https://ashwin.okta.com/oauth2/default',
-    clientId: '0oay7najdfoctpPO50h7',
     // redirectUri: window.location.origin + '/login/callback',
 
 };
@@ -22,13 +21,6 @@ const oktaSignInConfig = {
     features: {
         rememberMe: false
     }
-    // idps: [
-    //     {type: "GOOGLE", id: "0oaz5t6tyubdTIue70h7", text: "google"},     
-    //     {type: "FACEBOOK", id: "0oaz463iwpC1jc29f0h7"},       
-    //     {type: "CUSTOM", "text": "AWS Cognito", id: "0oazp0mqdhPVlWgwx0h7", className: 'social-auth-facebook-button'},   
-    //     {type: "CUSTOM", "text": "SAML", id: "0oa10e48d88qoGAKM0h8"}
-    // ],
-    // Additional documentation on config options can be found at https://github.com/okta/okta-signin-widget#basic-config-options
 };
   
 export { oktaAuthConfig, oktaSignInConfig };
